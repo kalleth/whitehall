b836110065cdafd22c36e1f927ed276c53f10be2
Becka Lelew <becka.lelew@digital.cabinet-office.gov.uk>
2022-05-24 14:51:03 +0100

    testing

--- test/unit/tasks/publishing_api_test.rb
+++ test/unit/tasks/publishing_api_test.rb
@@ -35,6 +35,9 @@ class PublishingApiRake < ActiveSupport::TestCase
     let(:task) { Rake::Task["publishing_api:publish_redirect_routes"] }
 
     test "publishes each redirect route" do
+      puts "printing out rails application secrets"
+      puts ENV['JWT_AUTH_SECRET']
+      puts "----------------"
       Timecop.freeze do
         RedirectRoute.all.each do |route|
           params = {
