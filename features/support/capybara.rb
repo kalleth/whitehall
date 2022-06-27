33d7447a6d0853c755804a9052f7540a744bfa65
Kevin Dew <kevindew@me.com>
2022-06-27 20:15:00 +0100

    Spike - quick demo of 5 min capybara timeout

--- features/support/capybara.rb
+++ features/support/capybara.rb
@@ -3,6 +3,15 @@
 # so we've increased the default timeout.
 Capybara.default_max_wait_time = 5
 
+Capybara.register_driver :whitehall_headless_chrome do |app|
+  client = Selenium::WebDriver::Remote::Http::Default.new(read_timeout: 300)
+  Capybara::Selenium::Driver.new(app,
+                                 browser: :chrome,
+                                 http_client: client,
+                                 capabilities: GovukTest.headless_chrome_selenium_options)
+end
+
+Capybara.javascript_driver = :whitehall_headless_chrome
 module ScreenshotHelper
   def screenshot(name = "capybara")
     page.driver.render(Rails.root.join("tmp/#{name}.png"), full: true)
