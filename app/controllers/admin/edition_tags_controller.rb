079fc85006a997d20705de5a52e88730a39272b7
hannako <jessica.jones@digital.cabinet-office.gov.uk>
2022-07-01 16:51:26 +0100

    Remove controller logic that auto tags to specialist topics

--- app/controllers/admin/edition_tags_controller.rb
+++ app/controllers/admin/edition_tags_controller.rb
@@ -17,19 +17,6 @@ class Admin::EditionTagsController < Admin::BaseController
       previous_version: params["taxonomy_tag_form"]["previous_version"],
     )
 
-    # Only automatically add legacy associations if this is new
-    # content, to avoid repeatedly adding legacy associations if
-    # they've been manually removed
-    unless @edition.published?
-      legacy_associations_changed = TaxonsToLegacyAssociationsTagging.new.call(
-        edition: @edition,
-        user: current_user,
-        selected_taxons: selected_taxons,
-      )
-
-      flash[:legacy_associations_changed] = legacy_associations_changed
-    end
-
     redirect_to redirect_path,
                 notice: "The tags have been updated."
   rescue GdsApi::HTTPConflict
@@ -40,11 +27,7 @@ class Admin::EditionTagsController < Admin::BaseController
 private
 
   def redirect_path
-    if params[:save]
-      admin_edition_path(@edition)
-    else
-      edit_admin_edition_legacy_associations_path(@edition, return: :tags)
-    end
+    admin_edition_path(@edition) if params[:save]
   end
 
   def enforce_permissions!
